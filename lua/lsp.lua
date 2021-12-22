ee1f91e772e559e2d5dfc806d360509eb1309f4b
Viet Nguyen <me@tuanviet.ru>
2021-12-22 18:17:08 +0300

    fix: remove duplicate LSP setup

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,3 @@
-require'lspconfig'.solargraph.setup{}
-require'lspconfig'.sumneko_lua.setup{}
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
@@ -11,23 +8,15 @@ lsp_installer.on_server_ready(function(server)
         capabilities = capabilities,
     }
 
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
     if server.name == "sumneko_lua" then
-        -- only apply these settings for the "sumneko_lua" server
         opts.settings = {
             Lua = {
                 diagnostics = {
-                    -- Get the language server to recognize the 'vim', 'use' global
                     globals = {'vim', 'use'},
                 },
                 workspace = {
-                    -- Make the server aware of Neovim runtime files
                     library = vim.api.nvim_get_runtime_file("", true),
                 },
-                -- Do not send telemetry data containing a randomized but unique identifier
                 telemetry = {
                     enable = false,
                 },
