c16352be837a3466c97ffd2671c468fbc279f220
Viet Nguyen <me@tuanviet.ru>
2021-12-22 18:24:46 +0300

    feat: use TAB to navigate buffers

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,3 +13,6 @@ vim.opt.smartindent = true
 
 vim.g.python3_host_prog = '~/.pyenv/versions/neovim3/bin/python'
 vim.g.python_host_prog = '~/.pyenv/versions/neovim2/bin/python'
+
+vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprev<CR>', { noremap = true })
